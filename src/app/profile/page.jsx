eeaaad376b2006123b92c14dc93db2518cedbbed
xyryc <amdanikzz@gmail.com>
2025-01-23 01:24:26 +0600

    added kinde auth

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div className="text-center">
+      <div className="p-6 border max-w-md mx-auto rounded-md my-10">
+        <h2 className="">Welcome to your profile!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,35 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import Link from "next/link";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+  console.log(user);
+
+  return (
+    <div className="border-b-2">
+      <div className="flex items-center justify-between py-6  container mx-auto">
+        <div>Read Next</div>
+
+        <div className="flex items-center gap-4">
+          <Link href="/">Home</Link>
+          <Link href="/profile">Profile</Link>
+
+          {user ? (
+            <>
+              <Link href="/api/auth/logout">Log Out</Link>
+            </>
+          ) : (
+            <>
+              <Link href="/api/auth/login">Log In</Link>
+              <Link href="/api/auth/register">Register</Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
