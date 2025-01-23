db9758a8f14baf1dca279bc3185b4657c9181ae2
xyryc <amdanikzz@gmail.com>
2025-01-23 16:01:17 +0600

    added post details page

--- src/app/blog/[postId]/page.jsx
+++ src/app/blog/[postId]/page.jsx
@@ -5,7 +5,20 @@ const DynamicPost = async ({ params }) => {
   const singlePost = await res.json();
   console.log(singlePost);
 
-  return <div>wadawd</div>;
+  return (
+    <div className="max-w-screen-sm mx-auto border p-6 rounded-md my-16">
+      <h2 className="capitalize text-2xl font-bold border-b-2 pb-4 mb-4">
+        {singlePost?.title}
+      </h2>
+
+      <p className="font-mono leading-8">{singlePost?.body}</p>
+
+      <div className="border-t-2 mt-4 pt-4 grid grid-cols-2">
+        <p className="text-sm font-serif font-thin">Posted by UserId: {singlePost?.userId}</p>
+        <p className="text-sm font-serif font-thin">Post Id: {singlePost?.id}</p>
+      </div>
+    </div>
+  );
 };
 
 export default DynamicPost;
